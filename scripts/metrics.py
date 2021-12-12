8909d6254a038fd930c6f9f9cb355a7b45a85f54
Limão <dev@lmnice.me>
2021-12-12 01:58:28 -0300

    Atualizar módulo scripts
    * adicionar funções de plots e metricas

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -2,7 +2,16 @@ import numpy as np
 import pandas as pd
 
 from typing import Tuple
-from sklearn.metrics import mean_absolute_percentage_error as fmape, mean_squared_error as fmse, mean_absolute_error as fmae, r2_score as fr2, f1_score as ff1, precision_score as fprecision, recall_score as frecall, accuracy_score as faccuracy
+from sklearn.metrics import mean_absolute_percentage_error as fmape, \
+                            mean_squared_error as fmse, \
+                            mean_absolute_error as fmae, \
+                            r2_score as fr2, \
+                            f1_score as ff1, \
+                            precision_score as fprecision, \
+                            recall_score as frecall, \
+                            accuracy_score as faccuracy, \
+                            roc_auc_score as fauroc, \
+                            confusion_matrix, cohen_kappa_score
 
 def tunar_hiperparams(
     tipo_modelo, 
@@ -11,13 +20,14 @@ def tunar_hiperparams(
     X_test: np.array or pd.DataFrame or pd.Series,
     y_train: np.array or pd.Series,
     y_test: np.array or pd.Series,
+    fixed_hiperparams: dict or None = None,
     tipo_dado: str = 'quantitativo',
+    metricas: dict or list or None = None,
     scaler = None,
-    *func_metricas_args,
-    **func_metricas_kwargs
+    func_metricas_kwargs: dict = dict()
 ) -> pd.DataFrame:
 
-    if tipo_dado not in [ 'quantitativo', 'qualitativo']:
+    if metricas is None and tipo_dado not in [ 'quantitativo', 'qualitativo']:
         raise NameError("'tipo_dado' deve ser 'qualitativo' ou 'quantitativo'.")
 
     if scaler is not None:
@@ -25,60 +35,195 @@ def tunar_hiperparams(
         X_train = sc.fit_transform(X_train)
         X_test = sc.transform(X_test)
 
+    if fixed_hiperparams is None:
+        fixed_hiperparams = dict()
     # create test cases
 
     comb_idx = pd.MultiIndex.from_product(selec_hiperparams)
 
+    dict_metricas = {
+        'mape': fmape,
+        'rmse': lambda *args, **kwargs: fmse(squared = False, *args, **kwargs),
+        'mae': fmae,
+        'r2': fr2,
+        'f1': ff1,
+        'recall': frecall,
+        'precision': fprecision,
+        'auroc': fauroc,
+    }
+
+    # dicionário de métricas
+    if metricas is None:
+        if tipo_dado == 'quantitativo':
+            metricas = {
+                'mape': fmape,
+                'rmse': lambda *args, **kwargs: fmse(squared = False, *args, **kwargs),
+                'mae': fmae,
+                'r2': fr2
+            }
+            
+        else:
+            metricas = {
+                'auroc': fauroc,
+                'f1': ff1,
+                'recall': frecall,
+                'precision': fprecision
+            }
+    
+    elif isinstance(metricas, list):
+        metricas_ = { nome_metrica: dict_metricas[nome_metrica] for nome_metrica in metricas }
+        metricas = metricas_
+
+    # create dataframe
+    score_cols = ['modelo'] + list(metricas.keys())
+
     # create apply function
-    def calc_scores(s, nomes_hiperparams):
+    def calc_scores(s, nomes_hiperparams, fixed_hiperparams = fixed_hiperparams, metricas = metricas):
         modelo_hiperparams = { nome: valor for nome, valor in zip(nomes_hiperparams, s.name)}
+        modelo_hiperparams.update(fixed_hiperparams)
         
         modelo = tipo_modelo(**modelo_hiperparams)
+
         modelo.fit(X_train, y_train)
-        s['modelos'] = modelo
+        s['modelo'] = modelo
         
         y_pred = modelo.predict(X_test)
 
-        metricas_kwargs = dict(
-            y_true = y_test,
-            y_pred = y_pred
-        )
+        metricas_args = ( y_test, y_pred )
 
-        metricas_kwargs.update(func_metricas_kwargs)
+        #metricas_args.update(func_metricas_kwargs)
 
-        if tipo_dado == 'quantitativo':
-
-            r2 = fr2(*func_metricas_args, **metricas_kwargs)
-            mape = fmape(*func_metricas_args, **metricas_kwargs)
-            rmse = fmse(squared = False, *func_metricas_args, **metricas_kwargs)
-            mae = fmae(*func_metricas_args, **metricas_kwargs)
-            
-            s['r2'] = r2
-            s['mape'] = mape
-            s['rmse'] = rmse
-            s['mae'] = mae
+        for col, fcalc in metricas.items():
+            s[col] = fcalc(*metricas_args, **func_metricas_kwargs)
         
-        else:
-            f1 = ff1(*func_metricas_args, **metricas_kwargs)
-            recall = frecall(*func_metricas_args, **metricas_kwargs)
-            precision = fprecision(*func_metricas_args, **metricas_kwargs)
+        # # metricas
 
-            s['f1'] = f1
-            s['precision'] = precision
-            s['recall'] = recall
+        # if tipo_dado == 'quantitativo':
+
+        #     if metricas is None:
+        #         metricas = {
+        #             'mape': fmape,
+        #             'rmse': lambda *args, **kwargs: fmse(squared = False, *args, **kwargs),
+        #             'mae': fmae,
+        #             'r2': fr2
+        #         }
+
+        #     r2 = fr2(**metricas_kwargs)
+        #     mape = fmape(**metricas_kwargs)
+        #     rmse = fmse(squared = False, **metricas_kwargs)
+        #     mae = fmae(**metricas_kwargs)
+            
+        #     s['r2'] = r2
+        #     s['mape'] = mape
+        #     s['rmse'] = rmse
+        #     s['mae'] = mae
+        
+        # else:
+        #     f1 = ff1(**metricas_kwargs)
+        #     recall = frecall(**metricas_kwargs)
+        #     precision = fprecision(**metricas_kwargs)
+
+        #     s['f1'] = f1
+        #     s['precision'] = precision
+        #     s['recall'] = recall
 
         return s
-        
-    # create dataframe
-    if tipo_dado == 'quantitativo':
-        score_cols = [ 'modelos', 'mape', 'rmse', 'mae', 'r2']
-        ascending = False
-    else:
-        score_cols = [ 'modelos', 'f1', 'recall', 'precision']
-        ascending = False
 
     scores = pd.DataFrame([], columns = score_cols, index = comb_idx)
     comb_nomes = scores.index.names
-    scores = scores.apply(calc_scores, nomes_hiperparams = comb_nomes, axis = 1)
-    scores = scores.sort_values(by = list(scores.columns[1:]), ascending = ascending)
+    scores = scores.apply(calc_scores, nomes_hiperparams = comb_nomes, fixed_hiperparams = fixed_hiperparams, metricas = metricas, axis = 1)
+    scores = scores.sort_values(by = list(scores.columns[1:]), ascending = False)
     return scores
+
+def calc_confusion_matrix(threshold, y_true, y_prob, beta = 1):
+    y_pred = (y_prob > threshold.values[0]) * 1
+    [[tn, fp], [fn, tp]] = confusion_matrix(y_true = y_true, y_pred = y_pred)
+
+    s = pd.Series([tn, fp, fn, tp], index = ['tn', 'fp', 'fn', 'tp'])
+
+    return s
+
+def calc_cohen_kappa_score(y_true, y_prob, threshold):
+    y_pred = y_prob > threshold
+    ck = cohen_kappa_score(y1 = y_true, y2 = y_pred)
+    return ck
+
+def calc_threshold_optimization(y_true, y_prob, beta = 1, calcs = dict()):
+
+    # generate threshold dataframe
+    thresholds = pd.DataFrame(np.linspace(0, 1, 101), columns = ['threshold'])
+
+    # calc tn, fp, fn, tp for each threshold
+    threshold_conf_mat = thresholds.apply(
+            calc_confusion_matrix, 
+            y_true = y_true, y_prob = y_prob, 
+            axis = 1
+        )
+
+    threshold_opt = pd.concat([
+        thresholds,
+        threshold_conf_mat
+    ], axis = 1)
+
+    # calc a few other metrics
+    threshold_opt['precision'] = (
+        (threshold_opt.tp / (threshold_opt.tp + threshold_opt.fp))
+            .where(threshold_opt.tp + threshold_opt.fp > 0, 0)
+    )
+
+    threshold_opt['recall'] = (
+        (threshold_opt.tp / (threshold_opt.tp + threshold_opt.fn))
+            .where(threshold_opt.tp + threshold_opt.fn > 0, 0)
+    )
+
+    threshold_opt['accuracy'] = (
+        (threshold_opt.tp + threshold_opt.tn) /
+        (threshold_opt.tn + threshold_opt.fp + threshold_opt.fn + threshold_opt.tp)
+    )
+
+    threshold_opt[f'f{beta:n}_score'] = (
+        (
+            (1+beta**2) * threshold_opt.precision * threshold_opt.recall /
+            (threshold_opt.precision * beta**2 + threshold_opt.recall)
+        )
+            .where(
+                threshold_opt.precision * beta**2 + threshold_opt.recall > 0,
+                0
+            )
+    )
+
+    threshold_opt['cohen_kappa_score'] = np.vectorize(
+        lambda threshold: calc_cohen_kappa_score(
+            y_true = y_true,
+            y_prob = y_prob,
+            threshold = threshold)
+    )(threshold_opt['threshold'])
+
+    for name, fun_config in calcs.items():
+        fcalc = fun_config['fcalc']
+        kwds = fun_config['kwds']
+
+        fapply = lambda row: fcalc(row, **kwds)
+
+        threshold_opt[name] = threshold_opt.apply(fapply, axis = 1)
+
+        try:
+            if fun_config['normalize']:
+                colvals = threshold_opt[name]
+                threshold_opt[name] = (colvals - colvals.min()) / (colvals.max() - colvals.min())
+        except KeyError:
+            pass
+
+    # return the dataframe (index is threshold level)
+    return threshold_opt.set_index('threshold')
+
+# cost function calculated over the confusim matrix entries
+def calc_confusion_matrix_cost(row, cost_matrix):
+    tn = row['tn']
+    fp = row['fp']
+    fn = row['fn']
+    tp = row['tp']
+    
+    each_cost = np.asarray([[tn, fp], [fn, tp]]) * np.asarray(cost_matrix)
+
+    return each_cost.sum()
--- scripts/plots.py
+++ scripts/plots.py
@@ -2,7 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import mean_absolute_percentage_error as fmape, mean_squared_error as fmse, mean_absolute_error as fmae, r2_score as fr2, f1_score as ff1, precision_score as fprecision, recall_score as frecall, accuracy_score as faccuracy
+
+from sklearn.metrics import mean_absolute_percentage_error as fmape, \
+                            mean_squared_error as fmse, \
+                            mean_absolute_error as fmae, \
+                            r2_score as fr2, \
+                            f1_score as ff1, \
+                            precision_score as fprecision, \
+                            recall_score as frecall, \
+                            accuracy_score as faccuracy, \
+                            roc_auc_score as fauroc, \
+                            roc_curve, \
+                            brier_score_loss as fbrier, \
+                            log_loss as flogloss
+
+from sklearn.utils.validation import check_is_fitted
+
+from sklearn.exceptions import NotFittedError
+
+from .metrics import calc_threshold_optimization
 
 def plot_true_pred(
     modelos, 
@@ -13,7 +31,7 @@ def plot_true_pred(
     # Vamos criar um gráfico para comparar os Valores Reais com os Preditos
     fig = plt.figure(figsize=(10, 8), dpi = 120)
 
-    for i, modelo in enumerate(modelos):
+    for i, (nome_modelo, modelo) in enumerate(modelos.items()):
         y_pred = modelo.predict(X_test)    
 
         # metricas
@@ -27,7 +45,7 @@ def plot_true_pred(
         rmse = fmse(squared = False, **metricas_kwargs)
         mae = fmae(**metricas_kwargs)
 
-        label = str(modelo)
+        label = nome_modelo
         label += f'\n(R² = {r2:.3%}, MAPE = {mape:.3%}, RMSE = {rmse:.2f}, MAE = {mae:.2f})'
 
         cor = f'C{i}'
@@ -46,3 +64,200 @@ def plot_true_pred(
     plt.legend()
     plt.show()
     return fig
+
+def plot_roc_auc_curve(models, X_test, y_test, X_train = None, y_train = None):
+    
+    models = models.copy()
+
+    fig, ax = plt.subplots()
+
+    model_idx = pd.Index([ model_name for model_name in models.keys() ], name = 'model_name')
+    model_df = pd.DataFrame([], index = model_idx, columns = ['model', 'auroc', 'brier', 'logloss', 'f1', 'precision', 'recall', 'accuracy'])
+    
+    for model_name, model in models.items():
+        
+        model_df.at[model_name, 'model'] = model
+
+        try:
+            check_is_fitted(model)
+        except NotFittedError:
+            model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
+        y_prob = model.predict_proba(X_test)[:, 1]
+        auc = fauroc(y_test, y_prob)
+
+        model_df.at[model_name, 'auroc'] = auc
+        model_df.at[model_name, 'accuracy'] = faccuracy(y_test, y_pred)
+        model_df.at[model_name, 'f1'] = ff1(y_test, y_pred)
+        model_df.at[model_name, 'precision'] = fprecision(y_test, y_pred)
+        model_df.at[model_name, 'recall'] = frecall(y_test, y_pred)
+        model_df.at[model_name, 'brier'] = fbrier(y_test, y_prob)
+        model_df.at[model_name, 'logloss'] = flogloss(y_test, y_prob)
+
+    model_df = model_df.sort_values(by = list(model_df.columns[1:]), ascending = False)
+    
+    for name, (model, auc) in model_df[['model', 'auroc']].iterrows():
+        y_prob = model.predict_proba(X_test)[:, 1]
+        fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+        ax.plot(fpr, tpr, label=f'{name} ROC (AUC = {auc:.2%})')
+
+    ax.plot([0, 1], [0, 1], linestyle = '--', color = 'gray', alpha = 0.8)
+    ax.set_xlim([-0.02, 1.0])
+    ax.set_ylim([0.0, 1.02])
+
+    pctformatter = lambda x, pos: f'{x:.0%}'
+
+    ax.set_xlabel('False Positive Rate\nFall-out\n1 - Recall of negative class')
+    ax.xaxis.set_major_formatter(pctformatter)
+    
+    ax.set_ylabel('True Positive Rate\nRecall of positive class')
+    ax.yaxis.set_major_formatter(pctformatter)
+    
+    ax.set_title('Receiver Operating Characteristic Curve')
+    ax.legend(loc="lower right")
+    plt.tight_layout()
+    plt.show()
+
+    return model_df.sort_values(by = list(model_df.columns[1:]), ascending = False)
+
+def plot_precision_recall_curve(
+    y_true: np.array or pd.Series, 
+    y_prob: np.array or pd.Series, 
+    threshold_opt: pd.DataFrame or None = None
+):
+
+    if threshold_opt is None:
+        threshold_opt = calc_threshold_optimization(
+            y_true = y_true, 
+            y_prob = y_prob
+        )
+
+    # plotar o gráfico precision recall curve
+    fig, ax = plt.subplots()
+
+    # ligar o grid e configurá-lo
+    plt.minorticks_on()
+    ax.grid(alpha = 0.2, which = 'major', linestyle = '--')
+    ax.grid(alpha = 0.2, which = 'minor', linestyle = '--')
+
+    ax.scatter(threshold_opt.index, threshold_opt.precision, label = 'Precision')
+    ax.scatter(threshold_opt.index, threshold_opt.recall, label = 'Recall')
+
+    fmtr = lambda x, pos: f'{x:.0%}'
+    ax.xaxis.set_major_formatter(fmtr)
+    ax.yaxis.set_major_formatter(fmtr)
+
+    ax.set_xlabel('Classification threshold')
+    ax.set_title('Precision / Recall - Threshold Curve')
+
+    ax.set_xlim([-0.01, 1.01])
+    ax.set_ylim([-0.01, 1.01])
+
+    plt.legend()
+    plt.show()
+
+    return fig
+
+def plot_threshold_curve(
+    y_true: np.array or pd.Series, 
+    y_prob: np.array or pd.Series, 
+    metrics: str or list or dict,
+    best_threshold: bool = True,
+    minimize: bool = False,
+    threshold_opt: pd.DataFrame or None = None):
+
+    if threshold_opt is None:
+        threshold_opt = calc_threshold_optimization(
+            y_true = y_true, 
+            y_prob = y_prob
+        )
+    
+
+    # plotar o gráfico com threshold no eixo X e a metrica/estimador no eixo Y
+    fig, ax = plt.subplots()
+
+    # ligar o grid e configurá-lo
+    plt.minorticks_on()
+    ax.grid(alpha = 0.2, which = 'major', linestyle = '--')
+    ax.grid(alpha = 0.2, which = 'minor', linestyle = '--')
+
+    if isinstance(metrics, str): # nome de uma coluna só
+        ax.scatter(threshold_opt.index, threshold_opt[metrics], label = metrics)
+        metric_label = metrics
+
+        axtitle = f"Threshold optimization curve - '{metric_label}'"
+
+        ax.set_ylabel(metric_label)
+
+        # plotar uma linha vertical com a otimização do threshold (maximização ou minimização)
+        if best_threshold:
+            if minimize:
+                best_threshold_x = threshold_opt.index[threshold_opt[metrics].argmin()]
+            else:
+                best_threshold_x = threshold_opt.index[threshold_opt[metrics].argmax()]
+
+            # plotar a linha vertical no melhor threshold em vermelho
+            ax.axvline(
+                x = best_threshold_x,
+                color = 'red',
+                linestyle = '--',
+                label = fr'Best $\theta$ = {best_threshold_x:.1%}'
+            )
+    
+    elif isinstance(metrics, list) or isinstance(metrics, dict):
+        # lista com metricas ou dsicionário com chave = nome da coluna e valor = nome da metrica
+        
+        axtitle = f"Threshold optimization curve\n"
+
+        # para cada metrica (funciona para lista e para as chaves de um dicionário)
+        for i, metric in enumerate(metrics):
+
+            try:
+                # se for um dicionário, o valor é o nome da métrica
+                name_metric = metrics[metric]
+            except TypeError:
+                # se não, o nome da métrica é o próprio nome do campo
+                name_metric = metric
+            
+            # plotar os pontos
+            color = f'C{i}'
+            ax.scatter(threshold_opt.index, threshold_opt[metric], 
+                label = name_metric, color = color
+            )
+            
+            axtitle += f"'{name_metric}', "
+
+            # plotar uma linha vertical com a otimização do threshold (maximização ou minimização)
+            if best_threshold:
+                if minimize:
+                    best_threshold_x = threshold_opt.index[threshold_opt[metric].argmin()]
+                else:
+                    best_threshold_x = threshold_opt.index[threshold_opt[metric].argmax()]
+
+                # plotar a linha vertical no melhor threshold com a mesma cor da metrica correspondente
+                ax.axvline(
+                    x = best_threshold_x,
+                    color = color,
+                    linestyle = '--',
+                    label = fr"Best $\theta$, '{name_metric}' = {best_threshold_x:.1%}"
+                )
+        
+        # remover a vírgula extra
+        axtitle = axtitle[:-2]
+        ax.set_ylabel('Metric')
+
+    # boniteza do gráfico
+    fmtr = lambda x, pos: f'{x:.0%}'
+    ax.xaxis.set_major_formatter(fmtr)
+
+    ax.set_xlabel(r'Classification threshold ($\theta$)')
+    ax.set_xlim([-0.01, 1.01])
+    
+    ax.set_title(axtitle)
+
+    plt.legend()
+    plt.show()
+
+    return fig
+
